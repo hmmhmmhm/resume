5381fc118007417a752ef4471eaee06d8b0cb2f8
hmmhmmhm <hmmhmmhm@naver.com>
2025-10-28 01:11:59 +0900

    refactor: simplify site structure by removing Spline animation
    - Removed Spline-related dependencies and components (splinetool/react-spline package)
    - Deleted unused signature.splinecode asset file
    - Consolidated routing by removing separate home.astro and resume.astro pages
    - Updated index page to directly show resume content instead of animation
    - Improved page metadata with more detailed description

--- apps/resume/src/components/spline-viewer.tsx
+++
@@ -1,9 +0,0 @@
-import Spline from "@splinetool/react-spline";
-
-interface SplineViewerProps {
-  scene: string;
-}
-
-export default function SplineViewer({ scene }: SplineViewerProps) {
-  return <Spline scene={scene} />;
-}
